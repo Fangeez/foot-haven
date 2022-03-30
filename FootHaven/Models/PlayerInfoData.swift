0ba145ff15bee47f13341104c0403e9259ef14e8
Fangeez <fangeez100@gmail.com>
2022-03-30 11:13:25 -0400

    Create data model for player query

---
+++ FootHaven/Models/PlayerInfoData.swift
@@ -0,0 +1,13 @@
+//  Created by Harris Dawurang on 3/30/22.
+
+struct PlayerInfoData: Codable {
+    let id: Int
+    let firstname: String
+    let lastname: String
+    let nationality: String
+    let age: Int
+    let height: String
+    let weight: String
+    let photo: String?
+}
+
---
+++ FootHaven/Models/PlayerStatsData.swift
@@ -0,0 +1,43 @@
+//  Created by Harris Dawurang on 3/30/22.
+
+struct PlayerStatsData: Codable {
+    let team: Team
+    let goals: Goals
+    let games: Games
+    let tackles: Tackles
+    let dribbles: Dribbles
+    let passes: Passes
+}
+
+struct Team: Codable {
+    let id: Int
+    let name: String
+    let logo: String?
+}
+
+struct Goals: Codable {
+    let total: Int
+    let assists: Int
+    let conceded: Int?
+    let saved: Int?
+}
+
+struct Games: Codable {
+    let appearances: Int
+    let minutes: Int
+}
+
+struct Tackles: Codable {
+    let total: Int
+    let interceptions: Int
+}
+
+struct Dribbles: Codable {
+    let attempts: Int
+    let success: Int
+}
+
+struct Passes: Codable {
+    let total: Int
+    let key: Int
+}
