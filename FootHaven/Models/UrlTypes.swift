2b606e160ac2658d59639229badf14638ddeb1dc
Fangeez <fangeez100@gmail.com>
2022-03-31 00:59:59 -0400

    Create Network Data Source for retrieving player, team, and league statistics

---
+++ FootHaven/Models/UrlTypes.swift
@@ -0,0 +1,7 @@
+//  Created by Harris Dawurang on 3/30/22.
+
+enum UrlTypes: String {
+    case player = "https://v3.football.api-sports.io/players"
+    case team = "https://v3.football.api-sports.io/teams/statistics"
+    case league = "https://v3.football.api-sports.io/standings"
+}
