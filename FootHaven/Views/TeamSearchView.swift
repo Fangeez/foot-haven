594ce9d07bc85da01fb5f1f32a73b442c268d6d3
Fangeez <fangeez100@gmail.com>
2022-04-04 14:10:15 -0400

    Add new assets and change homeview to tab view

---
+++ FootHaven/Views/TeamSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  TeamSearchView.swift
+//  FootHaven
+//
+//  Created by Harris Dawurang on 4/4/22.
+//
+
+import SwiftUI
+
+struct TeamSearchView: View {
+    var body: some View {
+        Text("Team Search Here")
+    }
+}
+
+struct TeamSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamSearchView()
+    }
+}
