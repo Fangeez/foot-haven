f1c3c9fb68f3c4f1cdb0aca975fa82ac92def59a
Fangeez <fangeez100@gmail.com>
2022-04-04 14:11:26 -0400

    Lint fix

--- FootHaven/Views/WelcomeView.swift
+++ FootHaven/Views/WelcomeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct WelcomeView: View {
     @State private var selection = 0
-
+    
     var body: some View {
         TabView(selection: $selection) {
             PlayerSearchView()
@@ -34,6 +34,6 @@ struct WelcomeView: View {
 
 struct WelcomeView_Previews: PreviewProvider {
     static var previews: some View {
-       WelcomeView()
+        WelcomeView()
     }
 }
